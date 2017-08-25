a85bfb2d127cc9c32afa81399f155cea131ea11f
Eetusav <eetu.savolainen@helsinki.fi>
2017-08-25 14:14:59 +0300

    SwitchBackToDefaultProfile

--- src/main/java/wad/config/SecurityConfiguration.java
+++ src/main/java/wad/config/SecurityConfiguration.java
@@ -5,7 +5,12 @@
  */
 package wad.config;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
@@ -24,6 +29,35 @@ import wad.auth.JpaAuthenticationProvider;
 @Configuration
 @EnableWebMvcSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private static final String HEROKU_DATABASE_URL_KEY = "DATABASE_URL";
+    private static final String HEROKU_POSTGRE_URL_PREFIX = "jdbc:postgresql://";
+    private static final String POSTGRESQL_DRIVER_CLASS = "org.postgresql.Driver";
+
+//    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv(HEROKU_DATABASE_URL_KEY));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = HEROKU_POSTGRE_URL_PREFIX + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+//        PoolProperties pp = new PoolProperties();
+//        pp.setDriverClassName(POSTGRESQL_DRIVER_CLASS);
+//        pp.setInitialSize(1);
+//        pp.setPassword(password);
+//        pp.setUsername(username);
+//        pp.setUrl(dbUrl);
+//        org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource(pp);
+//
+//        return dataSource;
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
